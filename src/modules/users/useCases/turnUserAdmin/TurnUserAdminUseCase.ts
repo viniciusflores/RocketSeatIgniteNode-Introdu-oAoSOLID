89d2774eef69868b6ce13e51bc29f955d9f96abf
Vinicius Flores <viniciusflores379@gmail.com>
2021-05-09 00:24:55 -0300

    :sparkles: Complete content of methods

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminUseCase.ts
@@ -9,7 +9,15 @@ class TurnUserAdminUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User {
-    // Complete aqui
+    const userExists = this.usersRepository.findById(user_id);
+
+    if (!userExists) {
+      throw new Error("User dont exists");
+    }
+
+    const userAdmin = this.usersRepository.turnAdmin(userExists);
+
+    return userAdmin;
   }
 }
 
